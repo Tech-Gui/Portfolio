5f095c5d3884c09dfb999cd20d6086c6d713f64b
Isaiah Tadiwanashe Chiraira <itchiraira@gmail.com>
2022-07-13 19:24:43 +0200

    Added the hero section

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // Lets do this
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+// Home endpoint
+app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+  const index = path.join(__dirname, 'views', 'index.html')
+  res.sendFile(index)
+})
+
+app.listen(port, () => {
+  console.log('Express server running on port', port)
+})
