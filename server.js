56ff527be68e777cc000118d796112d6615a6ef1
Isaiah Chiraira <itchiraira.com>
2023-04-14 10:54:49 +0200

    added CV

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 // Lets do this
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
 
 // Home endpoint
-app.use(express.static(path.join(__dirname, '/public')))
-app.get('/', (req, res) => {
-  const index = path.join(__dirname, 'views', 'index.html')
-  res.sendFile(index)
-})
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/", (req, res) => {
+  const index = path.join(__dirname, "views", "index.html");
+  res.sendFile(index);
+});
 
 // About endpoint
-app.use(express.static(path.join(__dirname, '/public')))
-app.get('/About', (req, res) => {
-  const index = path.join(__dirname, 'views', 'About.html')
-  res.sendFile(index)
-})
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/About", (req, res) => {
+  const index = path.join(__dirname, "views", "About.html");
+  res.sendFile(index);
+});
+
+// Resume endpoint
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/Resume", (req, res) => {
+  const index = path.join(__dirname, "views", "Resume.html");
+  res.sendFile(index);
+});
 
 // comingsoon endpoint
-app.use(express.static(path.join(__dirname, '/public')))
-app.get('/comingsoon', (req, res) => {
-  const index = path.join(__dirname, 'views', 'comingsoon.html')
-  res.sendFile(index)
-})
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/comingsoon", (req, res) => {
+  const index = path.join(__dirname, "views", "comingsoon.html");
+  res.sendFile(index);
+});
 
 app.listen(port, () => {
-  console.log('Express server running on port', port)
-})
+  console.log("Express server running on port", port);
+});
